085a9a174eeca8ab8a792d19db25273d67430e82
richielybmp <richielybmp@hotmail.com>
2017-09-23 12:12:16 -0300

    Aprendendo REST API com Json

--- HelloRestApi/Program.cs
+++ HelloRestApi/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace HelloRestApi
 {
-    class Program
+    public class Program
     {
+        private const string URL = "https://jsonplaceholder.typicode.com/comments";
+        private const string PARAMETROS = "";
+
         static void Main(string[] args)
         {
+            Program.PrepararRestAPI();
+        }
+
+        private static void PrepararRestAPI()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(PARAMETROS).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                var comentarios = response.Content.ReadAsAsync<IEnumerable<Comentario>>().Result;
+                Console.WriteLine("Listando items consumidos");
+                foreach (var comentario in comentarios)
+                {
+                    Console.WriteLine("PostId: {0}", comentario.postId);
+                    Console.WriteLine("Id: {0}", comentario.id);
+                    Console.WriteLine("Nome: {0}", comentario.name);
+                    Console.WriteLine("Email: {0}", comentario.email);
+                    Console.WriteLine("Body: {0}", comentario.body);
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            Console.ReadLine();
         }
     }
 }
